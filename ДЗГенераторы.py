ef27a22d19b1a0b84c61fe99fed7aa0de4d0d2f3
NataLltoffi <susnat114@gmail.com>
2024-12-19 17:36:23 +0300

    Add files via upload

---
+++ ДЗГенераторы.py
@@ -0,0 +1,13 @@
+def all_variants(text):
+    # Опишите логику работы внутри функции all_variants.
+    for x in range(len(text)):
+        for r in range(len(text) - x):
+            yield text[x:r + x + 1]
+
+
+# Вызовите функцию all_variants и выполните итерации.
+
+a = all_variants("abc")
+for i in a:
+    print(i)
+    
